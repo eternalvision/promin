d3d1869d5c2610f397b723db18e3ba660d708566
Sasha Priadchenko <56647239+eternalvision@users.noreply.github.com>
2022-10-27 12:29:42 +0200

    added music player

--- src/components/blocks/ResableComponents/Footer/Footer.js
+++ src/components/blocks/ResableComponents/Footer/Footer.js
@@ -1,8 +1,35 @@
-import React from "react";
-// import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import ReactJkMusicPlayer from "react-jinke-music-player";
+import "react-jinke-music-player/assets/index.css";
+// import track from "./For L (Tape Version).mp3";
 
 function Footer() {
-  return <footer className="Footer">footer</footer>;
+  //
+  //   const [audioLists, setAudioLists] = useState([
+  //   { musicSrc: '' },
+  //   ])
+
+  //   useEffect(() => {
+  //   setTimeout(() => {
+  //     setAudioLists([{ musicSrc: '' }])
+  //   }, 1000)
+  // }, [setAudioLists])
+
+  return (
+    <footer className="Footer">
+      <ReactJkMusicPlayer
+        autoPlay={true}
+        audioLists={[{ src: "./ForLTapeVersion.mp3" }]}
+        showMediaSession
+        mode="full"
+        showDestroy={false}
+        toggleMode={false}
+        showThemeSwitch={false}
+        theme="light"
+        showDownload={false}
+      />
+    </footer>
+  );
 }
 
 export default Footer;
